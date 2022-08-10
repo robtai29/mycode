cdbc2b5733a6ac427d2e263cfdc43ef186a31704
Robert Tai <robtai29@users.noreply.github.com>
2022-08-10 22:02:35 +0000

    8.10

---
+++ myList/listOfLists.py
@@ -0,0 +1,29 @@
+list1 = ["carrots", "beer", "beef"]
+list2 = ["steak", "hashbrowns", "eggs"]
+list3 = ["beers", "wine", "soda"]
+
+list4 = []
+list4.append(list1)
+list4.append(list2)
+list4.append(list3)
+print(list4)
+
+print("my fav food is : " + list4[2][0])
+
+#bigheros
+
+heroes = { "batman" : "batmobile", "superman" : "flying", "spiderman" : "climbing"}
+
+print(heroes.keys())
+print(heroes.values())
+
+batman = {"car" : "batmobile", "weapon" : "batgun"}
+joker = {"car" : "jokerMobile", "weapon" : "pokercard"}
+ironman = {"car" : "audi s8", "weapon" : "ironsuit"}
+
+print(batman.keys())
+
+print(batman.values())
+
+
+
