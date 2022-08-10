04ef0367ab7d0784b86aea07de39c2b027ad955c
Robert Tai <robtai29@users.noreply.github.com>
2022-08-10 15:05:35 +0000

    lab26

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+
+    # collect string input from the user
+    user_input = input("Please enter an IPv4 IP address:")
+    vendor = input("please enter vendor info");    
+    
+    print("You told me the IPv4 address is:" + user_input + " vendor info " + vendor)
+
+main()
+
