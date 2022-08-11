eaec226f2254cdff18ea1d1899ec72ec593cf132
Robert Tai <robtai29@users.noreply.github.com>
2022-08-11 16:21:55 +0000

    8.11

---
+++ iftest/condition01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+hostname = input("Whut value should we set for hostname?")
+
+if hostname.lower() == "mtg":
+    print("Thde hostname was found to be mtg")
+    print("hosetname matches expected config")
+
+else:
+    print("huh wrong")
+
+print("Exiting the script")
+
